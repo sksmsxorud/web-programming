df9a6648411e9f37fb75fd88ef2a2ebfa70b1bac
태굥 <146691296+sksmsxorud@users.noreply.github.com>
2024-04-19 15:58:23 +0900

    api 수정

--- app/main.py
+++ app/main.py
@@ -6,10 +6,10 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCd=11&cityCd=110&bizCd=C&apiKey=xxx&returnType=json"
-
+    URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCd=11&cityCd=110&apiKey=4r58D6g4lQs7vPoHBPUNbKf2ngyy11261I38bf65&returnType=json"
     contents = requests.get(URL).text
-    return{ "message": contents}
+
+    return { "message": contents}
 
 @app.get("/home") 
 def home():
