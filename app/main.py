48c022bd98138f056267b3a33b9c88a32f4e9aae
태굥 <146691296+sksmsxorud@users.noreply.github.com>
2024-04-19 15:24:15 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,6 +6,3 @@ app = FastAPI()
 def root():
     return {"message":"Hello Root!"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None);
-    return {"item_id": item_id, "q": q}
